7018d9ae2fdec167a88150a1131593817047ee23
David-Salazar-1082 <164534046+David-Salazar-1082@users.noreply.github.com>
2024-09-06 12:28:32 -0600

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,9 @@
+## Ejemplo de conjuntos
+
+paises = {"Estados Unidos", "Paises Bajos", "Japon"}
+print(paises)
+
+paises.add("Inglaterra")
+
+for maspaises in paises:
+    print(maspaises)
---
+++ diccionario.py
@@ -0,0 +1,8 @@
+datos = {
+    "Nombre": "David",
+    "Apellido": "Salazar",
+    "Correo": "a.22308051281082@cbtis128.edu.mx"
+}
+print(datos)
+for misdatos in datos:
+    print(misdatos)
